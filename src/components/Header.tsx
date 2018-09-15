6da1750f4edb90b9cbae52a5f4e04ac293529cf5
Matt <klai670@aucklanduni.ac.nz>
2018-09-15 23:41:53 +1200

    Add project files and folders

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { IndexLinkContainer } from "react-router-bootstrap";
+import { Link } from 'react-router-dom';
+
+export const Header: React.StatelessComponent<{}> = () => {
+    return (
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link to="/">dankNotDank</Link>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <IndexLinkContainer to="/FirstComponent">
+                    <NavItem>Page 1</NavItem>
+                </IndexLinkContainer>
+            </Nav>
+        </Navbar>
+    );
+}
